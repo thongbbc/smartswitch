a900c124b0057747b480742f92b78bd24ea50780
nhoangphung92 <nhoangphung@tma.com.vn>
2017-10-21 15:52:15 +0700

    update

--- myApp/App.js
+++ myApp/App.js
@@ -2,6 +2,50 @@ import React from 'react';
 import { StyleSheet, Text,TouchableHighlight,Animated,FlatList,View,Dimensions,Image } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import {StackNavigator,DrawerNavigator} from 'react-navigation'
+import { Client, Message } from 'react-native-paho-mqtt';
+
+//Set up an in-memory alternative to global localStorage
+const myStorage = {
+  setItem: (key, item) => {
+    myStorage[key] = item;
+  },
+  getItem: (key) => myStorage[key],
+  removeItem: (key) => {
+    delete myStorage[key];
+  },
+};
+
+// Create a client instance
+const client = new Client({ uri: 'ws://iot.eclipse.org:80/ws', clientId: 'clientId', storage: myStorage });
+
+// set event handlers
+client.on('connectionLost', (responseObject) => {
+  if (responseObject.errorCode !== 0) {
+    console.log(responseObject.errorMessage);
+  }
+});
+client.on('messageReceived', (message) => {
+  console.log(message.payloadString);
+});
+
+// connect the client
+client.connect()
+  .then(() => {
+    // Once a connection has been made, make a subscription and send a message.
+    console.log('onConnect');
+    return client.subscribe('/data');
+  })
+  .then(() => {
+    const message = new Message('Hello');
+    message.destinationName = '/data';
+    client.send(message);
+  })
+  .catch((responseObject) => {
+    if (responseObject.errorCode !== 0) {
+      console.log('onConnectionLost:' + responseObject.errorMessage);
+    }
+  })
+;
 class MyNotificationsScreen extends React.Component {
   static navigationOptions = {
     drawerLabel: 'Notifications',
